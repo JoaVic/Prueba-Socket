f7fd7dff29903907986a9385ccadfa30dc60e4a2
renato <renatovillalta14@gmail.com>
2024-05-28 16:05:10 -0500

    primer commit

---
+++ cibertec/src/main/java/com/cibertec/Client.java
@@ -0,0 +1,26 @@
+package com.cibertec;
+
+import java.net.Socket;
+
+public class Client {
+
+    private static final int PORT = 13;
+    private static final String HOST = "localhost";
+
+    public Client(){
+
+        try (Socket socket = new Socket(HOST, PORT)){
+            System.out.println("1 >> Connected to server on part " + PORT);
+            System.out.println(" 2 >> Complete");
+
+            socket.close();
+            
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    
+    public static void main(String[]args){
+        new Client();
+    }
+}
